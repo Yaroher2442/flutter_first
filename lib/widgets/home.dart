9dc8214f2baffc508ca11ee019d4ce9e73ae753e
yaroha24 <yaroher2442@gamail.con>
2021-12-22 20:35:48 +0300

    First steps

---
+++ lib/widgets/home.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import '../items/doble_select.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: [DoubleUpperButton()],
+        ),
+        body: const Center(child: Text("center")),
+        bottomNavigationBar:
+            BottomNavigationBar(items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.access_alarms), label: '&&&&&&&&&&&')
+        ]));
+  }
+}
